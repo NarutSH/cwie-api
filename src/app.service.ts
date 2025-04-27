3a85f3943ea86cc96265ed78e033b4d22b400a61
NarutSH <narut.sh@gmail.com>
2025-04-27 22:34:14 +0700

    feat : step v

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! V0.0.1';
+    return 'Hello World! V0.0.2';
   }
 }
